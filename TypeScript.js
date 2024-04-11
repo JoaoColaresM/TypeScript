12b06bc8778dfdf9d432647bcf309d7821ee0a65
João Colares <joaocolaresm@gmail.com>
2024-04-11 02:02:53 -0300

    primeiro

---
+++ TypeScript.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(x, y) {
+    return x * y;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicar(4, 7));
+console.log(saudacao('Antônio'));
---
+++ TypeScript.ts
@@ -0,0 +1,10 @@
+function multiplicar (x: number, y: number): number {
+    return x * y
+}
+
+function saudacao (nome: string): string {
+    return `Olá ${nome}`
+}
+
+console.log(multiplicar(4,7))
+console.log(saudacao('Antônio'))
